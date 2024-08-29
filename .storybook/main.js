8d7de086bc996306d16824f26c339f137a6dfd33
Anthony Cox <anthonyjohnc@gmail.com>
2024-08-29 20:00:11 +0100

    add a11y to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,12 +11,13 @@ const config = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-onboarding',
     "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-webpack5",
