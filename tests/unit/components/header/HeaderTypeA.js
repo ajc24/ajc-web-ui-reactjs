42d53a2461bf878872581f40a17fa15b9391b480
Anthony Cox <anthonyjohnc@gmail.com>
2024-08-31 14:04:51 +0100

    add unit tests for header type a component

--- tests/unit/components/header/HeaderTypeA.js
+++ tests/unit/components/header/HeaderTypeA.js
@@ -8,9 +8,9 @@ import { HeaderTypeA } from '../../../../src';
 import headerBackgroundTestImage from '../../../../stories/images/header-background-test.png';
 
 describe('Header Type A', () => {
-  describe('Header with no top border and transparent background (Default Settings)', () => {
+  describe('Header Type A with no top border (default), transparent background (default), left alignment (default)', () => {
     let componentDidMountSpy;
-    const testComponentId = 'test-default-header-id';
+    const testComponentId = 'test-header-border-none-bg-default-align-default';
     const testData = [];
 
     beforeAll(() => {
@@ -128,6 +128,103 @@ describe('Header Type A', () => {
 
     it('verifies that the "ajc-header-content-alignment-right" class is not assigned to the content element', () => {
       expect(testData[5].contains('ajc-header-content-alignment-right')).toBeFalsy();
+    });
+  });
+
+  describe('Header Type A with black top border (default colour), transparent background, left alignment', () => {
+    let componentDidMountSpy;
+    const testComponentId = 'test-header-border-default-bg-transparent-align-left';
+    const testData = [];
+
+    beforeAll(() => {
+      componentDidMountSpy = jest
+        .spyOn(HeaderTypeA.prototype, 'componentDidMount')
+        .mockImplementation(() => {});
+      const { unmount } = render(
+        <React.Fragment>
+          <HeaderTypeA id={testComponentId} showTopBorder={true} backgroundColour="transparent" alignment="left">
+            Header text content.
+          </HeaderTypeA>
+        </React.Fragment>
+      );
+      /* Build the DOM elements required for the tests */
+      const headerContainer = document.querySelector(`header[id="${testComponentId}--container"]`);
+      const headerContentContainer = document.querySelector(`div[id="${testComponentId}--content-container"]`);
+      const headerContent = document.querySelector(`div[id="${testComponentId}--content"]`);
+      
+      /* Verifies that the "ajc-header-border-black" class is assigned to the container element */
+      /* Verifies that the "ajc-header-border-grey" class is not assigned to the container element */
+      /* Verifies that the "ajc-header-border-red" class is not assigned to the container element */
+      /* Verifies that the "ajc-background-transparent" class is assigned to the container element */
+      /* Verifies that the "ajc-background-grey-1" class is not assigned to the container element */
+      /* Verifies that the "ajc-background-white" class is not assigned to the container element */
+      /* Verifies that the "ajc-background-yellow" class is not assigned to the container element */
+      testData.push(headerContainer.classList);
+
+      /* Verifies that the "ajc-header-content-container-with-border" class is assigned to the content container element */
+      /* Verifies that the "ajc-header-content-container-no-border" class is not assigned to the content container element */
+      testData.push(headerContentContainer.classList);
+
+      /* Verifies that the "ajc-header-content-alignment-left" class is assigned to the content element */
+      /* Verifies that the "ajc-header-content-alignment-center" class is not assigned to the content element */
+      /* Verifies that the "ajc-header-content-alignment-right" class is not assigned to the content element */
+      testData.push(headerContent.classList);
+
+      /* Unmount the component and clean up the test */
+      unmount();
+      cleanup();
+    });
+
+    afterAll(() => {
+      componentDidMountSpy.mockRestore();
+    });
+
+    it('verifies that the "ajc-header-border-black" class is assigned to the container element', () => {
+      expect(testData[0].contains('ajc-header-border-black')).toBeTruthy();
+    });
+
+    it('verifies that the "ajc-header-border-grey" class is not assigned to the container element', () => {
+      expect(testData[0].contains('ajc-header-border-grey')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-header-border-red" class is not assigned to the container element', () => {
+      expect(testData[0].contains('ajc-header-border-red')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-background-transparent" class is assigned to the container element', () => {
+      expect(testData[0].contains('ajc-background-transparent')).toBeTruthy();
+    });
+
+    it('verifies that the "ajc-background-grey-1" class is not assigned to the container element', () => {
+      expect(testData[0].contains('ajc-background-grey-1')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-background-white" class is not assigned to the container element', () => {
+      expect(testData[0].contains('ajc-background-white')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-background-yellow" class is not assigned to the container element', () => {
+      expect(testData[0].contains('ajc-background-yellow')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-header-content-container-with-border" class is assigned to the content container element', () => {
+      expect(testData[1].contains('ajc-header-content-container-with-border')).toBeTruthy();
+    });
+
+    it('verifies that the "ajc-header-content-container-no-border" class is not assigned to the content container element', () => {
+      expect(testData[1].contains('ajc-header-content-container-no-border')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-header-content-alignment-left" class is assigned to the content element', () => {
+      expect(testData[2].contains('ajc-header-content-alignment-left')).toBeTruthy();
+    });
+
+    it('verifies that the "ajc-header-content-alignment-center" class is not assigned to the content element', () => {
+      expect(testData[2].contains('ajc-header-content-alignment-center')).toBeFalsy();
+    });
+
+    it('verifies that the "ajc-header-content-alignment-right" class is not assigned to the content element', () => {
+      expect(testData[2].contains('ajc-header-content-alignment-right')).toBeFalsy();
     });
   });
 
