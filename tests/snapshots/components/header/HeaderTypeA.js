d4b0cb835f8d5f08606ddf694ade468eb8dfc059
Anthony Cox <anthonyjohnc@gmail.com>
2024-08-31 14:04:38 +0100

    add snapshot tests for header type a component

--- tests/snapshots/components/header/HeaderTypeA.js
+++ tests/snapshots/components/header/HeaderTypeA.js
@@ -20,9 +20,9 @@ describe('Header Type A', () => {
     componentDidMountSpy.mockRestore();
   });
 
-  describe('Header with no top border and transparent background (Default Settings)', () => {
+  describe('Header Type A with no top border (default), transparent background (default), left alignment (default)', () => {
     let snapshot;
-    const testComponentId = 'test-default-header-id';
+    const testComponentId = 'test-header-border-none-bg-default-align-default';
 
     beforeAll(() => {
       snapshot = TestDev.createSnapshot(
@@ -39,75 +39,22 @@ describe('Header Type A', () => {
     });
   });
 
-  // describe('Transferred props and rendering - Header with grey top border and white background', () => {
-  //   let jsonSnapshot;
+  describe('Header Type A with black top border (default colour), transparent background, left alignment', () => {
+    let snapshot;
+    const testComponentId = 'test-header-border-default-bg-transparent-align-left';
 
-  //   beforeAll(() => {
-  //     jsonSnapshot = TestDev.createSnapshot(
-  //       <React.Fragment>
-  //         <Header id="test-header-with-top-border-id" renderTopBorder={true} topBorderColour="grey" backgroundColour="white">
-  //           Header text content.
-  //         </Header>
-  //       </React.Fragment>
-  //     );
-  //   });
+    beforeAll(() => {
+      snapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <HeaderTypeA id={testComponentId} showTopBorder={true} backgroundColour="transparent" alignment="left">
+            Header text content.
+          </HeaderTypeA>
+        </React.Fragment>
+      );
+    });
 
-  //   it('verifies the snapshot for the component', () => {
-  //     expect(jsonSnapshot).toMatchSnapshot();
-  //   });
-  // });
-
-  // describe('Transferred props and rendering - Header with red top border and image background', () => {
-  //   let jsonSnapshot;
-
-  //   beforeAll(() => {
-  //     jsonSnapshot = TestDev.createSnapshot(
-  //       <React.Fragment>
-  //         <Header id="test-header-with-top-border-and-bg-image-id" renderTopBorder={true} topBorderColour="red" renderBackgroundImage={true} backgroundImageData={testImage}>
-  //           Header text content.
-  //         </Header>
-  //       </React.Fragment>
-  //     );
-  //   });
-
-  //   it('verifies the snapshot for the component', () => {
-  //     expect(jsonSnapshot).toMatchSnapshot();
-  //   });
-  // });
-
-  // describe('Transferred props and rendering - Header with no top border and grey background', () => {
-  //   let jsonSnapshot;
-
-  //   beforeAll(() => {
-  //     jsonSnapshot = TestDev.createSnapshot(
-  //       <React.Fragment>
-  //         <Header id="test-header-with-no-top-border-id" renderTopBorder={false} backgroundColour="grey">
-  //           Header text content.
-  //         </Header>
-  //       </React.Fragment>
-  //     );
-  //   });
-
-  //   it('verifies the snapshot for the component', () => {
-  //     expect(jsonSnapshot).toMatchSnapshot();
-  //   });
-  // });
-
-  // describe('Transferred props and rendering - Header with grey top border and yellow background', () => {
-  //   let jsonSnapshot;
-
-  //   beforeAll(() => {
-  //     jsonSnapshot = TestDev.createSnapshot(
-  //       <React.Fragment>
-  //         <Header id="test-header-with-top-border-id" renderTopBorder={true} topBorderColour="grey" backgroundColour="yellow">
-  //           Header text content.
-  //         </Header>
-  //       </React.Fragment>
-  //     );
-  //   });
-
-  //   it('verifies the snapshot for the component', () => {
-  //     expect(jsonSnapshot).toMatchSnapshot();
-  //   });
-  // });
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
 });
