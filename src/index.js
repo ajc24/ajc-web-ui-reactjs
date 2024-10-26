b05077eb90e9cc13deef8b7bd6f4401b812d3eee
Anthony Cox <anthonyjohnc@gmail.com>
2024-10-26 10:10:00 +0100

    add placeholder index js file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 /**
  * Developed by Anthony Cox in 2024
  */
-import {
-  FlexiImage,
-  FooterTypeA,
-  HeaderLogoTypeA,
-  HeaderTypeA,
-  MainContent,
-  MenuBarTypeA,
-} from './components';
-
-export {
-  FlexiImage,
-  FooterTypeA,
-  HeaderLogoTypeA,
-  HeaderTypeA,
-  MainContent,
-  MenuBarTypeA,
-};
