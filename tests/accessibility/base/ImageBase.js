5def54192c076d08e86f52ef6c385e676234dd58
Anthony Cox <anthonyjohnc@gmail.com>
2024-10-26 23:23:39 +0100

    add accessibility tests for image base component

---
+++ tests/accessibility/base/ImageBase.js
@@ -0,0 +1,81 @@
+/**
+ * Developed by Anthony Cox in 2024
+ */
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { TestDev } from 'ajc-testing-code';
+import ImageBase from '../../../src/components/base/ImageBase';
+import benjiLogo from '../../../src/components/images/files/logo-benji.png';
+
+describe('Image Base', () => {
+  const testTimeout = TestDev.getAxeTestTimeout();
+
+  afterEach(() => {
+    cleanup();
+    document.body.innerHTML = '';
+  });
+
+  describe('Decorative image with default parameters', () => {
+    let results;
+
+    beforeAll(async () => {
+      const { unmount } = render(
+        <div>
+          <header role="banner">
+            Header component
+          </header>
+          <div role="navigation">
+            Menu bar component
+          </div>
+          <main id="main-content" aria-label="Accessibility test">
+            <h1>Image Base Accessibility Test</h1>
+            <ImageBase id="decorative-image-1" src={benjiLogo} />
+          </main>
+          <footer role="contentinfo">
+            Footer component
+          </footer>
+        </div>
+      );
+      const html = TestDev.getComponentInHTMLTemplate('Image Base: Test 1');
+      unmount();
+      /* Perform the accessibility checks on this component */
+      results = await TestDev.runAxeCore(html);
+    }, testTimeout);
+
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
+    });
+  });
+
+  describe('Informative image with custom parameters', () => {
+    let results;
+
+    beforeAll(async () => {
+      const { unmount } = render(
+        <div>
+          <header role="banner">
+            Header component
+          </header>
+          <div role="navigation">
+            Menu bar component
+          </div>
+          <main id="main-content" aria-label="Accessibility test">
+            <h1>Image Base Accessibility Test</h1>
+            <ImageBase id="informative-image-1" src={benjiLogo} height={500} width={300} alt="Informative image alt text" />
+          </main>
+          <footer role="contentinfo">
+            Footer component
+          </footer>
+        </div>
+      );
+      const html = TestDev.getComponentInHTMLTemplate('Image Base: Test 2');
+      unmount();
+      /* Perform the accessibility checks on this component */
+      results = await TestDev.runAxeCore(html);
+    }, testTimeout);
+
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
+    });
+  });
+});
