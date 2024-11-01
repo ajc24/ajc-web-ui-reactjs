3cf2a956f3e2997136b7ec8acb15c2305a4f8b43
Anthony Cox <anthonyjohnc@gmail.com>
2024-11-01 07:55:12 +0000

    work in progress header component story

--- stories/header/Header.stories.js
+++ stories/header/Header.stories.js
@@ -22,19 +22,22 @@ const Template_Header = args => {
 }
 export const Default = Template_Header.bind({});
 Default.args = {
+  children: 'Default size Header component with custom content displayed',
   id: 'default-header',
   size: 'default',
 };
 
-export const WithGreyUpperBorder = Template_Header.bind({});
-WithGreyUpperBorder.args = {
+export const DefaultWithGreyUpperBorder = Template_Header.bind({});
+DefaultWithGreyUpperBorder.args = {
+  children: 'Default size Header component with a white background and grey upper border.',
   id: 'with-grey-upper-border',
   size: 'default',
   topBorder: 'grey',
 };
 
-export const WithRedUpperBorder = Template_Header.bind({});
-WithRedUpperBorder.args = {
+export const DefaultWithRedUpperBorder = Template_Header.bind({});
+DefaultWithRedUpperBorder.args = {
+  children: 'Default size Header component with a white background and red upper border.',
   id: 'with-red-upper-border',
   size: 'default',
   topBorder: 'red',
@@ -42,7 +45,29 @@ WithRedUpperBorder.args = {
 
 export const TallWithGreyBackground = Template_Header.bind({});
 TallWithGreyBackground.args = {
+  children: 'Tall size Header component with a grey background, no upper border.',
   backgroundColour: 'grey',
   id: 'with-grey-background',
   size: 'tall',
+};
+
+export const SmallWithTitleTextOnly = Template_Header.bind({});
+SmallWithTitleTextOnly.args = {
+  backgroundColour: 'grey',
+  id: 'with-title-text-only',
+  size: 'small',
+  titleText: 'Small size Header component with Title Text',
+  titleTextColour: 'black',
+};
+
+export const SmallWithTitleAndSubtitleTextOnly = Template_Header.bind({});
+SmallWithTitleAndSubtitleTextOnly.args = {
+  backgroundColour: 'grey',
+  headerTitleTextId: 'with-title-text-and-subtitle-text-only',
+  id: 'with-title-text-and-subtitle-text-only',
+  size: 'small',
+  subtitleText: 'Small size Header component with subtitle text',
+  subtitleTextColour: 'black',
+  titleText: 'Small size Header component with Title Text',
+  titleTextColour: 'black',
 };
