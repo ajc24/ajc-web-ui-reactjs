9d2378cdd661fb59ceda1314fc382c0c24b6abf9
Anthony Cox <anthonyjohnc@gmail.com>
2025-01-08 10:22:11 +0000

    add support for header background images

--- src/components/base/HeaderBase.js
+++ src/components/base/HeaderBase.js
@@ -15,49 +15,92 @@ const defaultComponentSize = 'default';
  * This baseline auto-handles screen widths from the most commonly used mobile screen sizes (360x800) to the most commonly used desktop sizes (1920x1080).
  * The default and small heights of the component is 160px. The tall height of the component is 320px.
  */
-const HeaderBase = props => {
-  const [id, setId] = useState(defaultComponentId);
-  const [size, setSize] = useState(defaultComponentSize);
+class HeaderBase extends React.Component {
+  /**
+   * Initialise the Header Base component
+   * @param {any} props 
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: defaultComponentId,
+      size: defaultComponentSize,
+    };
+    this.setId = this.setId.bind(this);
+    this.setSize = this.setSize.bind(this);
 
-  useEffect(() => {
+    /* Create the references for this component */
+    this.contentContainerRef = React.createRef();
+  }
+
+  componentDidMount() {
     /* Set the ID for the component */
-    if (props.id !== undefined) {
-      setId(`${props.id}--header-container`);
+    if (this.props.id !== undefined) {
+      this.setId(`${this.props.id}--header-container`);
     }
     /* Set the size for the component */
-    if (props.size !== undefined) {
-      if (props.size === 'default' || props.size === 'small' || props.size === 'tall') {
+    if (this.props.size !== undefined) {
+      if (this.props.size === 'default' || this.props.size === 'small' || this.props.size === 'tall') {
         /* Set the declared supported size for this component */
-        setSize(props.size);
+        this.setSize(this.props.size);
       } else {
         /* An invalid size was specified - set to default size */
-        setSize('default');
+        this.setSize('default');
       }
     }
     /* Setup the default CSS styling for the page which is rendering this component */
     PageTemplateConfig.setupDocumentBodyCss();
-  });
 
-  /* Set the CSS class lists for all DOM elements */
-  let headerContainerCss = 'header-container background-white';
-  if (size === 'tall') {
-    /* Set the taller header baseline to be rendered */
-    headerContainerCss += ' header-container-size-tall';
-  } else {
-    /* Set the smaller header baseline to be rendered as a default */
-    headerContainerCss += ' header-container-size-small';
+    /* Set the background image to the header component */
+    if (this.props.backgroundImageSrc !== undefined) {
+      this.contentContainerRef.current.style.backgroundImage = `url(${this.props.backgroundImageSrc})`;
+    }
   }
-  const headerContentContainerCss = 'header-content-container background-transparent';
 
-  return (
-    <header role="banner" id={id} className={headerContainerCss}>
-      <div className={headerContentContainerCss}>
-        {props.children}
-      </div>
-    </header>
-  );
+  /**
+   * Sets the ID for the base header component
+   * @param {string} newId 
+   */
+  setId(newId) {
+    this.setState({
+      id: newId,
+    });
+  }
+
+  /**
+   * Sets the size for the base header component
+   * @param {'default'|'small'|'tall'} newSize 
+   */
+  setSize(newSize) {
+    this.setState({
+      size: newSize,
+    });
+  }
+
+  render() {
+    /* Set the CSS class lists for all DOM elements */
+    let headerContainerCss = 'header-container background-white';
+    if (this.state.size === 'tall') {
+      /* Set the taller header baseline to be rendered */
+      headerContainerCss += ' header-container-size-tall';
+    } else {
+      /* Set the smaller header baseline to be rendered as a default */
+      headerContainerCss += ' header-container-size-small';
+    }
+    const headerContentContainerCss = 'header-content-container background-transparent';
+
+    return (
+      <header role="banner" id={this.state.id} className={headerContainerCss}>
+        <div className={headerContentContainerCss} ref={this.contentContainerRef}>
+          {this.props.children}
+        </div>
+      </header>
+    );
+  };
 }
 HeaderBase.propTypes = {
+  /** The background image data to be displayed. */
+  backgroundImageSrc: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]),
   /** The content to be displayed within the header template component. */
   children: PropTypes.any,
   /** The unique identifier for this component. */
