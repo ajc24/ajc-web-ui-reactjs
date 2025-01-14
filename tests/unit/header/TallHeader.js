4642488fdd80553bf09090a6eaf5512af8ac1a89
Anthony Cox <anthonyjohnc@gmail.com>
2025-01-14 12:58:31 +0000

    placeholder files for header unit tests

---
+++ tests/unit/header/TallHeader.js
@@ -0,0 +1,11 @@
+/**
+ * Developed by Anthony Cox in 2025
+ */
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { TallHeader } from '../../../src';
+import headerBgImage from '../../../stories/images/files/header-bg-image-sky.png';
+import storybookLogo from '../../../stories/images/files/storybook-logo-long-tall.svg';
+
+describe('Tall Header', () => {
+});
