e4deed1bc4d855e0ccde9cc907c974a60a4439f8
Anthony Cox <anthonyjohnc@gmail.com>
2025-02-14 22:12:15 +0000

    finalise full page template story

--- stories/mock-pages/FullPageTemplate.stories.js
+++ stories/mock-pages/FullPageTemplate.stories.js
@@ -16,13 +16,13 @@ export default {
  */
 const Template_FullPageTemplate = args => {
   return <React.Fragment>
-    <HeaderBase size={args.size ? args.size : 'small'} backgroundColour="grey" topBorder={args.topBorder ? args.topBorder : 'grey'}>
+    <HeaderBase size={args.size ? args.size : 'small'} backgroundColour={args.backgroundColour ? args.backgroundColour : 'grey'} topBorder={args.topBorder ? args.topBorder : 'grey'}>
       This component is designed to act as a template component for a header.
     </HeaderBase>
     <MenuBarBase backgroundColour={args.backgroundColour ? args.backgroundColour : 'grey'}>
       This component is designed to act as a baseline / template on which all menu bar components can be developed.
     </MenuBarBase>
-    <MainBase backgroundColour="grey" title={args.title}>
+    <MainBase backgroundColour={args.backgroundColour ? args.backgroundColour : 'grey'} title={args.title}>
       This component is designed to act as a baseline / template on which all main components can be developed.
     </MainBase>
     <FooterBase backgroundColour={args.backgroundColour ? args.backgroundColour : 'grey'}>
@@ -32,21 +32,28 @@ const Template_FullPageTemplate = args => {
 }
 export const Example1 = Template_FullPageTemplate.bind({});
 Example1.args = {
+  backgroundColour: 'white',
   title: 'Full page template example 1',
+  topBorder: 'off',
 };
 
 export const Example2 = Template_FullPageTemplate.bind({});
 Example2.args = {
-  backgroundColour: 'red',
-  size: 'small',
   title: 'Full page template example 2',
-  topBorder: 'red',
-}
+};
 
 export const Example3 = Template_FullPageTemplate.bind({});
 Example3.args = {
+  backgroundColour: 'red',
+  size: 'small',
+  title: 'Full page template example 3',
+  topBorder: 'red',
+}
+
+export const Example4 = Template_FullPageTemplate.bind({});
+Example4.args = {
   backgroundColour: 'green',
   size: 'tall',
-  title: 'Full page template example 3',
+  title: 'Full page template example 4',
   topBorder: 'green',
 }
