9d520df3bb9a7e5a1c7b55c34c369a8235479a16
Anthony Cox <anthonyjohnc@gmail.com>
2025-02-27 09:21:12 +0000

    add snapshots for header logo component

---
+++ tests/snapshots/header/HeaderLogo.js
@@ -0,0 +1,48 @@
+/**
+ * Developed by Anthony Cox in 2025
+ */
+import 'jsdom-global/register';
+import React from 'react';
+import { TestDev } from 'ajc-testing-code';
+import { HeaderLogo } from '../../../src';
+import storybookLogo from '../../../stories/images/files/storybook-logo.svg';
+import storybookLogoLong from '../../../stories/images/files/storybook-logo-long.svg';
+
+describe('Header Logo', () => {
+  /* Set the data for the tests */
+  const testComponentId = 'test-component-id';
+
+  describe('Logo image in small size Header with no border, left alignment, square logo type', () => {
+    let querySelectorSpy;
+    let snapshot;
+    
+    beforeAll(() => {
+      /* Create the spies for the test */
+      querySelectorSpy = jest
+        .spyOn(global.document, 'querySelector')
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if tall header component is rendered */
+          return null;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if top border is set to the header */
+          return null;
+        });
+
+      /* Generate the snapshot for the component */
+      snapshot = TestDev.createSnapshot_UseFakeTimers(
+        <React.Fragment>
+          <HeaderLogo id={testComponentId} src={storybookLogoLong} alignment="left" logoType="square" parentHeaderId="test-parent-header-id" />
+        </React.Fragment>
+      );
+    });
+
+    afterAll(() => {
+      querySelectorSpy.mockRestore();
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+});
