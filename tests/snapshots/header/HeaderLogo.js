95c05e494588475179a7e86c0a60d99e12304620
Anthony Cox <anthonyjohnc@gmail.com>
2025-03-01 15:30:56 +0000

    add snapshots for header logo component

--- tests/snapshots/header/HeaderLogo.js
+++ tests/snapshots/header/HeaderLogo.js
@@ -10,6 +10,9 @@ import storybookLogoLong from '../../../stories/images/files/storybook-logo-long
 
 describe('Header Logo', () => {
   /* Set the data for the tests */
+  const heightSubtitleTextElement = 35;
+  const heightTitleTextElement_NoWrapping = 75;
+  const heightTitleTextElement_WithWrapping = 95;
   const testComponentId = 'test-component-id';
 
   describe('Logo image in small size Header with no border, left alignment, square logo type', () => {
@@ -135,6 +138,245 @@ describe('Header Logo', () => {
       snapshot = TestDev.createSnapshot_UseFakeTimers(
         <React.Fragment>
           <HeaderLogo id={testComponentId} src={storybookLogoLong} alignment="centre" logoType="rectangle" parentHeaderId="test-parent-header-id" />
+        </React.Fragment>
+      );
+    });
+
+    afterAll(() => {
+      querySelectorSpy.mockRestore();
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Logo image in tall size Header with no border, left alignment, no title text, no subtitle text', () => {
+    let querySelectorSpy;
+    let snapshot;
+    
+    beforeAll(() => {
+      /* Create the spies for the test */
+      querySelectorSpy = jest
+        .spyOn(global.document, 'querySelector')
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if tall header component is rendered */
+          return document.createElement('div');
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if top border is set to the header */
+          return null;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if title text component is rendered */
+          return null;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if subtitle text component is rendered */
+          return null;
+        });
+
+      /* Generate the snapshot for the component */
+      snapshot = TestDev.createSnapshot_UseFakeTimers(
+        <React.Fragment>
+          <HeaderLogo id={testComponentId} src={storybookLogoLong} alignment="left" parentHeaderId="test-parent-header-id" />
+        </React.Fragment>
+      );
+    });
+
+    afterAll(() => {
+      querySelectorSpy.mockRestore();
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Logo image in tall size Header with border, centre alignment, no title text, no subtitle text', () => {
+    let querySelectorSpy;
+    let snapshot;
+    
+    beforeAll(() => {
+      /* Create the spies for the test */
+      querySelectorSpy = jest
+        .spyOn(global.document, 'querySelector')
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if tall header component is rendered */
+          return document.createElement('div');
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if top border is set to the header */
+          return true;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if title text component is rendered */
+          return null;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if subtitle text component is rendered */
+          return null;
+        });
+
+      /* Generate the snapshot for the component */
+      snapshot = TestDev.createSnapshot_UseFakeTimers(
+        <React.Fragment>
+          <HeaderLogo id={testComponentId} src={storybookLogoLong} alignment="centre" parentHeaderId="test-parent-header-id" />
+        </React.Fragment>
+      );
+    });
+
+    afterAll(() => {
+      querySelectorSpy.mockRestore();
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Logo image in tall size Header with no border, default alignment, with title text (no text wrapping), no subtitle text', () => {
+    let querySelectorSpy;
+    let snapshot;
+    
+    beforeAll(() => {
+      /* Create the spies for the test */
+      querySelectorSpy = jest
+        .spyOn(global.document, 'querySelector')
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if tall header component is rendered */
+          return document.createElement('div');
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if top border is set to the header */
+          return null;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if title text component is rendered */
+          return {
+            dataset: {
+              wrap: 'false',
+            },
+            getBoundingClientRect: () => {
+              return {
+                height: heightTitleTextElement_NoWrapping,
+              };
+            },
+          };
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if subtitle text component is rendered */
+          return null;
+        });
+
+      /* Generate the snapshot for the component */
+      snapshot = TestDev.createSnapshot_UseFakeTimers(
+        <React.Fragment>
+          <HeaderLogo id={testComponentId} src={storybookLogoLong} parentHeaderId="test-parent-header-id" />
+        </React.Fragment>
+      );
+    });
+
+    afterAll(() => {
+      querySelectorSpy.mockRestore();
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Logo image in tall size Header with no border, default alignment, with title text (no text wrapping), with subtitle text (visible)', () => {
+    let querySelectorSpy;
+    let snapshot;
+    
+    beforeAll(() => {
+      /* Create the spies for the test */
+      querySelectorSpy = jest
+        .spyOn(global.document, 'querySelector')
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if tall header component is rendered */
+          return document.createElement('div');
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if top border is set to the header */
+          return null;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if title text component is rendered */
+          return {
+            dataset: {
+              wrap: 'false',
+            },
+            getBoundingClientRect: () => {
+              return {
+                height: heightTitleTextElement_NoWrapping,
+              };
+            },
+          };
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if subtitle text component is rendered */
+          return {
+            getBoundingClientRect: () => {
+              return {
+                height: heightSubtitleTextElement,
+              };
+            },
+          };
+        });
+
+      /* Generate the snapshot for the component */
+      snapshot = TestDev.createSnapshot_UseFakeTimers(
+        <React.Fragment>
+          <HeaderLogo id={testComponentId} src={storybookLogoLong} parentHeaderId="test-parent-header-id" />
+        </React.Fragment>
+      );
+    });
+
+    afterAll(() => {
+      querySelectorSpy.mockRestore();
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(snapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Logo image in tall size Header with border, default alignment, with title text (with text wrapping), with subtitle text (hidden)', () => {
+    let querySelectorSpy;
+    let snapshot;
+    
+    beforeAll(() => {
+      /* Create the spies for the test */
+      querySelectorSpy = jest
+        .spyOn(global.document, 'querySelector')
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if tall header component is rendered */
+          return document.createElement('div');
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if top border is set to the header */
+          return true;
+        })
+        .mockImplementationOnce(() => {
+          /* handleImageSize() functionality - determine if title text component is rendered */
+          return {
+            dataset: {
+              wrap: 'true',
+            },
+            getBoundingClientRect: () => {
+              return {
+                height: heightTitleTextElement_WithWrapping,
+              };
+            },
+          };
+        });
+
+      /* Generate the snapshot for the component */
+      snapshot = TestDev.createSnapshot_UseFakeTimers(
+        <React.Fragment>
+          <HeaderLogo id={testComponentId} src={storybookLogoLong} parentHeaderId="test-parent-header-id" />
         </React.Fragment>
       );
     });
