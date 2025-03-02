2b75d7ac93dd75b2f1eb8b962f90cafa3625fefd
Anthony Cox <anthonyjohnc@gmail.com>
2025-03-02 19:41:03 +0000

    add unit tests for header component

--- tests/unit/header/Header.js
+++ tests/unit/header/Header.js
@@ -258,13 +258,14 @@ describe('Header', () => {
       /* Mount the component */
       const { unmount } = render(
         <React.Fragment>
-          <Header id={testComponentId} size="small" subtitleTextContent="Subtitle text content" topBorder="green-2" titleTextContent="Header Title Text Content" />
+          <Header id={testComponentId} size="small" subtitleTextColour="white" subtitleTextContent="Subtitle text content" topBorder="green-2"
+            titleTextContent="Header Title Text Content" />
         </React.Fragment>
       );
       /* Build the DOM elements required for the tests */
       const outerContent_topBorder = document.querySelector(`header[id="${testComponentId}--header-base"] > div[class*="header-border-top-green-2"]`);
       const headingElement = document.querySelector(`div[id="${testComponentId}--header-title-text"] > h1[class*="title-text-font-black"]`);
-      const paragraphElement = document.querySelector(`div[id="${testComponentId}--header-subtitle-text"] > p[class*="font-black"]`).textContent;
+      const paragraphElement = document.querySelector(`div[id="${testComponentId}--header-subtitle-text"] > p[class*="font-white"]`).textContent;
       
       /* Verifies that the top border is rendered in the outer content element */
       testData.push(outerContent_topBorder);
@@ -334,7 +335,7 @@ describe('Header', () => {
       /* Mount the component */
       const { unmount } = render(
         <React.Fragment>
-          <Header id={testComponentId} size="tall" subtitleTextColour="black" subtitleTextContent="Subtitle text content" topBorder="navy"
+          <Header id={testComponentId} size="tall" subtitleTextContent="Subtitle text content" topBorder="navy"
             titleTextColour="white" titleTextContent="Header Title Text Content" />
         </React.Fragment>
       );
@@ -367,6 +368,130 @@ describe('Header', () => {
 
     it('verifies that the subtitle text content is rendered', () => {
       expect(testData[2]).not.toBeNull();
+    });
+  });
+
+  describe('Small Header, with logo image, with title text, no subtitle text', () => {
+    const testData = [];
+
+    beforeAll(() => {
+      /* Mount the component */
+      const { unmount } = render(
+        <React.Fragment>
+          <Header id={testComponentId} size="small" logoSrc={storybookLogo} topBorder="red" titleTextContent="Header Title Text Content" />
+        </React.Fragment>
+      );
+      /* Build the DOM elements required for the tests */
+      const outerContent_topBorder = document.querySelector(`header[id="${testComponentId}--header-base"] > div[class*="header-border-top-red"]`);
+      const headingElement = document.querySelector(`div[id="${testComponentId}--header-title-text"] > h1`);
+      const logoImageElement = document.querySelector(`img[id="${testComponentId}--header-logo--decorative-image"]`);
+      const paragraphElement = document.querySelector(`div[id="${testComponentId}--header-subtitle-text"] > p`);
+      const titleTextContainerElement_NoLogo = document.querySelector('div[class*="header-title-subtitle-text-container-width-no-logo"]');
+      const titleTextContainerElement_WithLogo = document.querySelector('div[class*="header-title-subtitle-text-container-width-with-logo"]');
+      
+      /* Verifies that the top border is rendered in the outer content element */
+      testData.push(outerContent_topBorder);
+
+      /* Verifies that the logo image is rendered */
+      testData.push(logoImageElement);
+
+      /* Verifies that the title text content is rendered */
+      testData.push(headingElement);
+
+      /* Verifies that the "header-title-subtitle-text-container-width-no-logo" class is not set to the title and subtitle text container element */
+      testData.push(titleTextContainerElement_NoLogo);
+
+      /* Verifies that the "header-title-subtitle-text-container-width-with-logo" class is set to the title and subtitle text container element */
+      testData.push(titleTextContainerElement_WithLogo);
+
+      /* Verifies that the subtitle text content is not rendered */
+      testData.push(paragraphElement);
+
+      /* Unmount the component and clean up the test */
+      unmount();
+      cleanup();
+    });
+
+    it('verifies that the top border is rendered in the outer content element', () => {
+      expect(testData[0]).not.toBeNull();
+    });
+
+    it('verifies that the logo image is rendered', () => {
+      expect(testData[1]).not.toBeNull();
+    });
+
+    it('verifies that the title text content is rendered', () => {
+      expect(testData[2]).not.toBeNull();
+    });
+
+    it('verifies that the "header-title-subtitle-text-container-width-no-logo" class is not set to the title and subtitle text container element', () => {
+      expect(testData[3]).toBeNull();
+    });
+
+    it('verifies that the "header-title-subtitle-text-container-width-with-logo" class is set to the title and subtitle text container element', () => {
+      expect(testData[4]).not.toBeNull();
+    });
+
+    it('verifies that the subtitle text content is not rendered', () => {
+      expect(testData[5]).toBeNull();
+    });
+  });
+
+  describe('Small Header, with logo image, with title text, with subtitle text', () => {
+    const testData = [];
+
+    beforeAll(() => {
+      /* Mount the component */
+      const { unmount } = render(
+        <React.Fragment>
+          <Header id={testComponentId} size="small" logoSrc={storybookLogo} subtitleTextContent="Subtitle text content" titleTextContent="Header Title Text Content" />
+        </React.Fragment>
+      );
+      /* Build the DOM elements required for the tests */
+      const headingElement = document.querySelector(`div[id="${testComponentId}--header-title-text"] > h1`);
+      const logoImageElement = document.querySelector(`img[id="${testComponentId}--header-logo--decorative-image"]`);
+      const paragraphElement = document.querySelector(`div[id="${testComponentId}--header-subtitle-text"] > p[class*="font-black"]`);
+      const titleTextContainerElement_NoLogo = document.querySelector('div[class*="header-title-subtitle-text-container-width-no-logo"]');
+      const titleTextContainerElement_WithLogo = document.querySelector('div[class*="header-title-subtitle-text-container-width-with-logo"]');
+      
+      /* Verifies that the logo image is rendered */
+      testData.push(logoImageElement);
+
+      /* Verifies that the title text content is rendered */
+      testData.push(headingElement);
+
+      /* Verifies that the "header-title-subtitle-text-container-width-no-logo" class is not set to the title and subtitle text container element */
+      testData.push(titleTextContainerElement_NoLogo);
+
+      /* Verifies that the "header-title-subtitle-text-container-width-with-logo" class is set to the title and subtitle text container element */
+      testData.push(titleTextContainerElement_WithLogo);
+
+      /* Verifies that the subtitle text content is rendered */
+      testData.push(paragraphElement);
+
+      /* Unmount the component and clean up the test */
+      unmount();
+      cleanup();
+    });
+
+    it('verifies that the logo image is rendered', () => {
+      expect(testData[0]).not.toBeNull();
+    });
+
+    it('verifies that the title text content is rendered', () => {
+      expect(testData[1]).not.toBeNull();
+    });
+
+    it('verifies that the "header-title-subtitle-text-container-width-no-logo" class is not set to the title and subtitle text container element', () => {
+      expect(testData[2]).toBeNull();
+    });
+
+    it('verifies that the "header-title-subtitle-text-container-width-with-logo" class is set to the title and subtitle text container element', () => {
+      expect(testData[3]).not.toBeNull();
+    });
+
+    it('verifies that the subtitle text content is rendered', () => {
+      expect(testData[4]).not.toBeNull();
     });
   });
 });
