989a2b517a6fc16aa6c1171ab1e0f77176f5707f
Anthony Cox <anthonyjohnc@gmail.com>
2025-03-02 12:04:44 +0000

    add accessibility tests for the header component

--- tests/accessibility/header/Header.js
+++ tests/accessibility/header/Header.js
@@ -5,6 +5,8 @@ import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import { TestDev } from 'ajc-testing-code';
 import { Header } from '../../../src';
+import storybookLogo from '../../../stories/images/files/storybook-logo.svg';
+import storybookLogoLong from '../../../stories/images/files/storybook-logo-long.svg';
 
 describe('Header', () => {
   const testComponentId = 'test-header-id';
@@ -37,6 +39,96 @@ describe('Header', () => {
         </div>
       );
       const html = TestDev.getComponentInHTMLTemplate('Header: Test 1');
+      unmount();
+      /* Perform the accessibility checks on this component */
+      results = await TestDev.runAxeCore(html);
+    }, testTimeout);
+
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
+    });
+  });
+
+  describe('Small Header, with square logo image, no title text, no subtitle text', () => {
+    let results;
+
+    beforeAll(async () => {
+      /* Mount the component being accessibility tested */
+      const { unmount } = render(
+        <div>
+          <Header id={testComponentId} logoSrc={storybookLogo} logoType="square" size="small" />
+          <div role="navigation">
+            Menu bar component
+          </div>
+          <main id="main-content" aria-label="Accessibility test">
+            <h1>Header Accessibility Test</h1>
+          </main>
+          <footer role="contentinfo" id="footer">
+            Footer component
+          </footer>
+        </div>
+      );
+      const html = TestDev.getComponentInHTMLTemplate('Header: Test 2');
+      unmount();
+      /* Perform the accessibility checks on this component */
+      results = await TestDev.runAxeCore(html);
+    }, testTimeout);
+
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
+    });
+  });
+
+  describe('Small Header, with rectangle logo image, no title text, no subtitle text', () => {
+    let results;
+
+    beforeAll(async () => {
+      /* Mount the component being accessibility tested */
+      const { unmount } = render(
+        <div>
+          <Header id={testComponentId} logoSrc={storybookLogoLong} logoType="rectangle" size="small" />
+          <div role="navigation">
+            Menu bar component
+          </div>
+          <main id="main-content" aria-label="Accessibility test">
+            <h1>Header Accessibility Test</h1>
+          </main>
+          <footer role="contentinfo" id="footer">
+            Footer component
+          </footer>
+        </div>
+      );
+      const html = TestDev.getComponentInHTMLTemplate('Header: Test 3');
+      unmount();
+      /* Perform the accessibility checks on this component */
+      results = await TestDev.runAxeCore(html);
+    }, testTimeout);
+
+    it('verifies the accessibility standards for the component', () => {
+      expect(results).toBeTruthy();
+    });
+  });
+
+  describe('Tall Header, with logo image, no title text, no subtitle text', () => {
+    let results;
+
+    beforeAll(async () => {
+      /* Mount the component being accessibility tested */
+      const { unmount } = render(
+        <div>
+          <Header id={testComponentId} logoSrc={storybookLogoLong} size="tall" />
+          <div role="navigation">
+            Menu bar component
+          </div>
+          <main id="main-content" aria-label="Accessibility test">
+            <h1>Header Accessibility Test</h1>
+          </main>
+          <footer role="contentinfo" id="footer">
+            Footer component
+          </footer>
+        </div>
+      );
+      const html = TestDev.getComponentInHTMLTemplate('Header: Test 4');
       unmount();
       /* Perform the accessibility checks on this component */
       results = await TestDev.runAxeCore(html);
