f60bb591c866c2efa738c40515af30eb4066f429
Anthony Cox <anthonyjohnc@gmail.com>
2025-03-04 22:49:42 +0000

    add re-usable colour combinations data

---
+++ src/components/data/colour-combinations.js
@@ -0,0 +1,41 @@
+/**
+ * Developed by Anthony Cox in 2025
+ */
+const colourCombinations = {
+  gold: {
+    backgroundColour: 'gold',
+    fontColour: 'black',
+  },
+  green: {
+    backgroundColour: 'green',
+    fontColour: 'white',
+  },
+  'green-2': {
+    backgroundColour: 'green-2',
+    fontColour: 'black',
+  },
+  grey: {
+    backgroundColour: 'grey-2',
+    fontColour: 'white',
+  },
+  'navy-and-gold': {
+    backgroundColour: 'navy',
+    fontColour: 'gold',
+  },
+  'navy-and-white': {
+    backgroundColour: 'navy',
+    fontColour: 'white',
+  },
+  red: {
+    backgroundColour: 'red',
+    fontColour: 'white',
+  },
+  white: {
+    backgroundColour: 'white',
+    fontColour: 'black',
+  },
+};
+
+export {
+  colourCombinations,
+};
