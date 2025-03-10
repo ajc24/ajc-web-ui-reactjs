612d06211683e20143a1dfa78abba9d3fb784cb1
Anthony Cox <anthonyjohnc@gmail.com>
2025-03-10 22:02:22 +0000

    add re-usable function to determine background and font colours

--- src/components/data/colour-combinations.js
+++ src/components/data/colour-combinations.js
@@ -1,6 +1,8 @@
 /**
  * Developed by Anthony Cox in 2025
  */
+
+/* Colour combinations data */
 const colourCombinations = {
   gold: {
     backgroundColour: 'gold',
@@ -36,6 +38,27 @@ const colourCombinations = {
   },
 };
 
+/**
+ * Gets the chosen background and font colour combination based on the developers general colour
+ * selection. Defaults to a white background with black font.
+ * @param {string} chosenColour 
+ * @returns {{ backgroundColour: string, fontColour: string }}
+ */
+const getColourCombination = (chosenColour, defaultBackgroundColour = 'white', defaultFontColour = 'black') => {
+  if (colourCombinations[`${chosenColour}`] !== undefined) {
+    /* Return the chosen colour combination */
+    return {
+      backgroundColour: colourCombinations[`${chosenColour}`].backgroundColour,
+      fontColour: colourCombinations[`${chosenColour}`].fontColour,
+    };
+  }
+  /* Return the default colour combination */
+  return {
+    backgroundColour: defaultBackgroundColour,
+    fontColour: defaultFontColour,
+  };
+};
+
 export {
-  colourCombinations,
+  getColourCombination,
 };
