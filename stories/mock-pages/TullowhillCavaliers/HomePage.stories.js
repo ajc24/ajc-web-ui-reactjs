17c960da22ecb1edf2e0d38d8f035a3d2a813ce7
Anthony Cox <anthonyjohnc@gmail.com>
2025-03-14 08:25:47 +0000

    add mock menu bar data to the story

--- stories/mock-pages/TullowhillCavaliers/HomePage.stories.js
+++ stories/mock-pages/TullowhillCavaliers/HomePage.stories.js
@@ -12,6 +12,58 @@ export default {
 
 /* Data for use in the mock page */
 const headerLogoAltText = 'Logo image for Tullowhill Cavaliers showing two outlines of Benji, our ruby Cavalier dog and the Tullowhill Cavaliers title text below him.';
+const menuBarData = [
+  {
+    href: '#',
+    id: 'menu-item-link-1',
+    title: 'Menu Item Link 1',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-2',
+    title: 'Menu Item Link 2',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-3',
+    title: 'Menu Item Link 3',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-4',
+    title: 'Menu Item Link 4',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-5',
+    title: 'Menu Item Link 5',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-6',
+    title: 'Menu Item Link 6',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-7',
+    title: 'Menu Item Link 7',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-8',
+    title: 'Menu Item Link 8',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-9',
+    title: 'Menu Item Link 9',
+  },
+  {
+    href: '#',
+    id: 'menu-item-link-10',
+    title: 'Menu Item Link 10',
+  },
+];
 
 /**
  * Full page template for the Home page
@@ -21,7 +73,7 @@ const headerLogoAltText = 'Logo image for Tullowhill Cavaliers showing two outli
 const Template_HomePage = args => {
   return <React.Fragment>
     <Header backgroundColour="grey" id="home-page" logoAlt={headerLogoAltText} logoSrc={tullowhillCavaliersLogo} size="tall" topBorder="navy" />
-    <MenuBar id="home-page" backgroundColour="navy-and-gold" />
+    <MenuBar id="home-page" backgroundColour="navy-and-gold" menuItemsList={menuBarData} />
     <MainBase backgroundColour="grey" title="Tullowhill Cavaliers - Home Page">
       The main content for the current page will go here.
     </MainBase>
