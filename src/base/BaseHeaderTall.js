73921146c0598fd154d96fb1c3265ed20bf214b6
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-06 22:00:01 +0100

    finalise base header tall component

--- src/base/BaseHeaderTall.js
+++ src/base/BaseHeaderTall.js
@@ -1,11 +1,8 @@
 /**
  * Developed by Anthony Cox in 2025
  */
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import BaseHeader from './BaseHeader';
-
-const defaultId = 'default--base-header-tall';
 
 /**
  * Baseline component setting the template for all tall Header components to be developed from.
@@ -13,22 +10,8 @@ const defaultId = 'default--base-header-tall';
  * The default height of the component is 320px.
  */
 const BaseHeaderTall = props => {
-  useEffect(() => {
-    let currentId = defaultId;
-    if (props.id !== undefined) {
-      currentId = props.id;
-    }
-    /* Set the header element to a tall height */
-    const header = document.querySelector(`header[id="${currentId}"]`);
-    header.style.height = '320px';
-
-    /* Set the headers inner content element to a tall height, taking into account the 8px padding on the top and bottom */
-    const headerContentOuter = document.querySelector(`header[id="${currentId}"] > div > div`);
-    headerContentOuter.style.height = '304px';
-  });
-
   return (
-    <BaseHeader backgroundColour={props.backgroundColour} id={props.id !== undefined ? props.id : defaultId}>
+    <BaseHeader backgroundColour={props.backgroundColour} id={props.id !== undefined ? props.id : 'default--base-header-tall'} customSizeClass="header-size-tall">
       {props.children}
     </BaseHeader>
   );
