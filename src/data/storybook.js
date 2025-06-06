c957079148f31e99f61f8ff26f54143cf0491051
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-06 18:09:37 +0100

    add storybook data for body element class list handling

---
+++ src/data/storybook.js
@@ -0,0 +1,13 @@
+/**
+ * Developed by Anthony Cox in 2025
+ */
+
+/* List of Storybook-oriented CSS classes */
+const storybookCssClassList = [
+  'sb-main-padded',
+  'sb-show-main',
+];
+
+export {
+  storybookCssClassList,
+};
