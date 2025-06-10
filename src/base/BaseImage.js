25c438fb9a1941e5bc60dc38c790125ea80da4d3
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-10 07:15:44 +0100

    fix incorrect prop type declaration

--- src/base/BaseImage.js
+++ src/base/BaseImage.js
@@ -100,7 +100,7 @@ BaseImage.propTypes = {
   /** The alternate text to be attached to the image and read out by screen readers. */
   alt: PropTypes.string,
   /** The height of the image. */
-  height: PropTypes.oneOfType([ 'auto', PropTypes.number ]),
+  height: PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
   /** The unique identifier for this component. */
   id: PropTypes.string,
   /** The custom functionality to be executed when the mouse down on image event is fired. */
