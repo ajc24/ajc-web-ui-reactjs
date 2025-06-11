4ee7dc47f5df38cdbebfa11a41231d2ed6e8747e
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-11 20:39:09 +0100

    add initial viewports support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,15 @@
 /**
  * Developed by Anthony Cox in 2025
  */
+import { INITIAL_VIEWPORTS } from 'storybook/viewport';
 
 /**
  * @type { import('@storybook/react').Preview }
  */
 const preview = {
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    viewport: {
+      options: INITIAL_VIEWPORTS,
     },
   },
   tags: ['autodocs'],
