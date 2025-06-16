d6c9c2d5d671ca607d079005e29f542009f13304
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-16 07:44:40 +0100

    add story for menu bar item component

--- stories/menu-bar/MenuBarItem.stories.js
+++ stories/menu-bar/MenuBarItem.stories.js
@@ -18,6 +18,9 @@ const Template_MenuBarItem = args => {
   return <MenuBarItem {...args} />;
 }
 
+/**
+ * Menu Bar Item component designed to test component visibility toggle
+ */
 class Template_MenuBarItem_VisibilityToggle extends React.Component {
   constructor(props) {
     super(props);
@@ -34,7 +37,9 @@ class Template_MenuBarItem_VisibilityToggle extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <MenuBarItem backgroundColour="gold" href="#" id="visibility-toggle" isHidden={this.state.isHidden} />
+        <MenuBarItem backgroundColour="gold" href="#" id="visibility-toggle" isHidden={this.state.isHidden}>
+          Toggle Test
+        </MenuBarItem>
         <br />
         <button id="visibility-toggle" onClick={this.onClickToggleIsHidden}>Toggle Visibility</button>
       </React.Fragment>
@@ -42,13 +47,18 @@ class Template_MenuBarItem_VisibilityToggle extends React.Component {
   }
 }
 
+/**
+ * Menu Bar Item component template to be used to test the visibility toggle
+ * @param {*} args 
+ * @returns {React.Component}
+ */
 const Template_MenuBarItem_VisibilityToggle_Story = args => {
   return <Template_MenuBarItem_VisibilityToggle {...args} />;
 }
 
 export const Default = {
   args: {
-    children: '',
+    children: 'Default White',
     href: '#',
     id: 'default',
     isHidden: false,
@@ -59,7 +69,7 @@ export const Default = {
 export const WithGoldBackground = {
   args: {
     backgroundColour: 'gold',
-    children: '',
+    children: 'Gold',
     href: '#',
     id: 'with-gold-background',
     isHidden: false,
@@ -67,7 +77,85 @@ export const WithGoldBackground = {
   render: Template_MenuBarItem,
 };
 
-export const VisibilityTest = {
+export const WithGreenBackground = {
+  args: {
+    backgroundColour: 'green',
+    children: 'Green',
+    href: '#',
+    id: 'with-green-background',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+};
+
+export const WithGreyBackground = {
+  args: {
+    backgroundColour: 'grey',
+    children: 'Grey',
+    href: '#',
+    id: 'with-grey-background',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+};
+
+export const WithNavyAndGoldBackground = {
+  args: {
+    backgroundColour: 'navy-and-gold',
+    children: 'Navy and Gold',
+    href: '#',
+    id: 'with-navy-and-gold-background',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+};
+
+export const WithNavyAndWhiteBackground = {
+  args: {
+    backgroundColour: 'navy-and-white',
+    children: 'Navy and White',
+    href: '#',
+    id: 'with-navy-and-white-background',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+};
+
+export const WithRedBackground = {
+  args: {
+    backgroundColour: 'red',
+    children: 'Red',
+    href: '#',
+    id: 'with-red-background',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+};
+
+export const WithRightSideSpacing = {
+  args: {
+    addRightSideSpacing: true,
+    backgroundColour: 'green',
+    children: 'Spacing Right',
+    href: '#',
+    id: 'with-right-side-spacing',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+}
+
+export const WithTruncatedText = {
+  args: {
+    backgroundColour: 'navy-and-gold',
+    children: 'With Truncated Text Content',
+    href: '#',
+    id: 'with-truncated-text',
+    isHidden: false,
+  },
+  render: Template_MenuBarItem,
+};
+
+export const VisibilityToggleTest = {
   args: {},
   render: Template_MenuBarItem_VisibilityToggle_Story,
 };
