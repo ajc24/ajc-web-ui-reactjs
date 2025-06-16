c312c98154ffb5ae7bee34d93250cced9f1be9c5
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-16 23:41:33 +0100

    work in progress story for dropdown menu bar container component

--- stories/menu-bar/DropdownMenuBarContainer.stories.js
+++ stories/menu-bar/DropdownMenuBarContainer.stories.js
@@ -22,6 +22,74 @@ export const Default = {
   args: {
     id: 'default',
     isHidden: false,
+    left: 10,
+    top: 10,
+  },
+  render: Template_DropdownMenuBarContainer,
+};
+
+export const WithGoldBackground = {
+  args: {
+    backgroundColour: 'gold',
+    id: 'with-gold-background',
+    isHidden: false,
+    left: 10,
+    top: 10,
+  },
+  render: Template_DropdownMenuBarContainer,
+};
+
+export const WithGreenBackground = {
+  args: {
+    backgroundColour: 'green',
+    id: 'with-green-background',
+    isHidden: false,
+    left: 10,
+    top: 10,
+  },
+  render: Template_DropdownMenuBarContainer,
+};
+
+export const WithGreyBackground = {
+  args: {
+    backgroundColour: 'grey',
+    id: 'with-grey-background',
+    isHidden: false,
+    left: 10,
+    top: 10,
+  },
+  render: Template_DropdownMenuBarContainer,
+};
+
+export const WithNavyAndGoldBackground = {
+  args: {
+    backgroundColour: 'navy-and-gold',
+    id: 'with-navy-and-gold-background',
+    isHidden: false,
+    left: 10,
+    top: 10,
+  },
+  render: Template_DropdownMenuBarContainer,
+};
+
+export const WithNavyAndWhiteBackground = {
+  args: {
+    backgroundColour: 'navy-and-white',
+    id: 'with-navy-and-white-background',
+    isHidden: false,
+    left: 10,
+    top: 10,
+  },
+  render: Template_DropdownMenuBarContainer,
+};
+
+export const WithRedBackground = {
+  args: {
+    backgroundColour: 'red',
+    id: 'with-red-background',
+    isHidden: false,
+    left: 10,
+    top: 10,
   },
   render: Template_DropdownMenuBarContainer,
 };
