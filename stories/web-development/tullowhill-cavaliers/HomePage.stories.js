a0b905db62b95d0d52e940909331915b4c20eca9
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-16 09:40:33 +0100

    work in progress story menu bar implementation

--- stories/web-development/tullowhill-cavaliers/HomePage.stories.js
+++ stories/web-development/tullowhill-cavaliers/HomePage.stories.js
@@ -2,13 +2,31 @@
  * Developed by Anthony Cox in 2025
  */
 import React from 'react';
-import { HeaderTallLogo } from '../../../src';
+import { HeaderTallLogo, MenuBar } from '../../../src';
 import tullowhillCavaliersLogo from './images/tullowhill-cavaliers-logo.png';
 
 export default {
   tags: ['!autodocs'],
   title: 'Web Development/Tullowhill Cavaliers/Home Page',
 };
+
+const menuBarItemsList = [
+  {
+    href: '#',
+    id: 'menu-bar-item-1',
+    title: 'Item 1',
+  },
+  {
+    href: '#',
+    id: 'menu-bar-item-2',
+    title: 'Item 2',
+  },
+  {
+    href: '#',
+    id: 'menu-bar-item-3',
+    title: 'Item 3',
+  }
+];
 
 /**
  * Home Page template
@@ -21,6 +39,7 @@ const Template_HomePage = () => {
       alt="Logo image for Tullowhill Cavaliers showing two outlines of Benji, our ruby Cavalier dog and the Tullowhill Cavaliers title text below him."
       backgroundColour="grey" id="tullowhill-cavaliers-logo" src={tullowhillCavaliersLogo}
     />
+    <MenuBar backgroundColour="navy-and-gold" id="tullowhill-cavaliers" menuBarItemsList={menuBarItemsList} />
   </React.Fragment>;
 };
 
