d47c40e169f5edd2bdd6711fcde0ca248f3c7eb1
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-21 09:09:18 +0100

    work in progress story for dropdown menu bar item component

--- stories/menu-bar/DropdownMenuBarItem.stories.js
+++ stories/menu-bar/DropdownMenuBarItem.stories.js
@@ -37,8 +37,25 @@ const dropdownMenuBarItemData = {
       title: 'Dropdown Menu Bar Item 5',
     },
   ],
-  id: 'item-1',
-  title: 'Item 1',
+  id: 'menu-bar-item',
+  title: 'Menu Bar Item',
+};
+
+const dropdownMenuBarItemData_TruncatedText = {
+  dropdownMenuBarItemsList: [
+    {
+      href: '#',
+      id: 'dropdown-menu-bar-item-1',
+      title: 'Dropdown Menu Bar Item 1',
+    },
+    {
+      href: '#',
+      id: 'dropdown-menu-bar-item-2',
+      title: 'Dropdown Menu Bar Item 2',
+    },
+  ],
+  id: 'menu-bar-item',
+  title: 'With Truncated Text Content',
 };
 
 /**
@@ -93,6 +110,87 @@ export const DefaultWhite = {
     children: 'Default White',
     dropdownMenuBarItemData: dropdownMenuBarItemData,
     id: 'default-white',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithGoldBackground = {
+  args: {
+    backgroundColour: 'gold',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-gold-background',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithGreenBackground = {
+  args: {
+    backgroundColour: 'green',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-green-background',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithGreyBackground = {
+  args: {
+    backgroundColour: 'grey',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-grey-background',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithNavyAndGoldBackground = {
+  args: {
+    backgroundColour: 'navy-and-gold',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-navy-and-gold-background',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithNavyAndWhiteBackground = {
+  args: {
+    backgroundColour: 'navy-and-white',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-navy-and-white-background',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithRedBackground = {
+  args: {
+    backgroundColour: 'red',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-red-background',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+};
+
+export const WithRightSideSpacing = {
+  args: {
+    addRightSideSpacing: true,
+    backgroundColour: 'navy-and-white',
+    dropdownMenuBarItemData: dropdownMenuBarItemData,
+    id: 'with-right-side-spacing',
+    isHidden: false,
+  },
+  render: Template_DropdownMenuBarItem,
+}
+
+export const WithTruncatedText = {
+  args: {
+    backgroundColour: 'red',
+    dropdownMenuBarItemData: dropdownMenuBarItemData_TruncatedText,
+    id: 'with-truncated-text',
     isHidden: false,
   },
   render: Template_DropdownMenuBarItem,
