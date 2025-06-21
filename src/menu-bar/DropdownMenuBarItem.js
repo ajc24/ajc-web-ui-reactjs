f280949d70a3b8c09946210582964a8459cc7fc9
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-21 09:09:37 +0100

    work in progress dropdown menu bar item component

--- src/menu-bar/DropdownMenuBarItem.js
+++ src/menu-bar/DropdownMenuBarItem.js
@@ -10,6 +10,7 @@ import './css/menu-bar-common.css';
 import './css/menu-bar-dropdown-item.css';
 import './css/menu-bar-item.css';
 
+const maximumMenuBarItemButtonHeight = 55;
 
 class DropdownMenuBarItem extends React.Component {
   /**
@@ -22,14 +23,15 @@ class DropdownMenuBarItem extends React.Component {
       isHidden: true,
       isSelected: false,
     };
+    this.getArrowDOMElement = this.getArrowDOMElement.bind(this);
     this.getButtonDOMElement = this.getButtonDOMElement.bind(this);
-    // this.getSpanDOMElement = this.getSpanDOMElement.bind(this);
+    this.getTitleDOMElement = this.getTitleDOMElement.bind(this);
     this.getIdButtonDOMElement = this.getIdButtonDOMElement.bind(this);
     this.handleOnClickDropdownMenuBarItem = this.handleOnClickDropdownMenuBarItem.bind(this);
-    // this.handleOnKeyDown = this.handleOnKeyDown.bind(this);
-    // this.handleTextContentHeight = this.handleTextContentHeight.bind(this);
+    this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this);
+    this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+    this.handleTextContentHeight = this.handleTextContentHeight.bind(this);
     this.setIsHidden = this.setIsHidden.bind(this);
-    // this.setIsSelected = this.setIsSelected(this);
     this.setIsVisible = this.setIsVisible.bind(this);
   }
 
@@ -37,7 +39,7 @@ class DropdownMenuBarItem extends React.Component {
     /* Initialise all of the key parameters for this component */
     if (this.state.isHidden === false) {
       this.setIsVisible();
-      // this.handleTextContentHeight();
+      this.handleTextContentHeight();
     } else if (this.state.isHidden === true) {
       this.setIsHidden();
     }
@@ -48,7 +50,7 @@ class DropdownMenuBarItem extends React.Component {
       if (this.props.isHidden === false) {
         /* Mark the component as visible */
         this.setIsVisible();
-        // this.handleTextContentHeight();
+        this.handleTextContentHeight();
       } else if (this.props.isHidden === true) {
         /* Mark the component as hidden */
         this.setIsHidden();
@@ -57,11 +59,27 @@ class DropdownMenuBarItem extends React.Component {
   }
 
   /**
+   * Retrieves the dropdown menu bar items arrow element from the DOM
+   * @returns {HTMLElement}
+   */
+  getArrowDOMElement() {
+    return document.querySelector(`button[id="${this.getIdButtonDOMElement()}"] > span[class="dropdown-menu-bar-button-arrow"]`);
+  }
+
+  /**
    * Retrieves the dropdown menu bar items button element from the DOM
    * @returns {HTMLElement}
    */
   getButtonDOMElement() {
     return document.querySelector(`button[id="${this.getIdButtonDOMElement()}"]`);
+  }
+
+  /**
+   * Retrieves the dropdown menu bar items title element from the DOM
+   * @returns {HTMLElement}
+   */
+  getTitleDOMElement() {
+    return document.querySelector(`button[id="${this.getIdButtonDOMElement()}"] > span[class="dropdown-menu-bar-button-title"]`);
   }
 
   /**
@@ -87,13 +105,13 @@ class DropdownMenuBarItem extends React.Component {
    */
   handleOnClickDropdownMenuBarItem(event) {
     event.preventDefault();
+    alert('clicked');
     if (this.state.isHidden === false) {
       if (this.state.isSelected === false) {
         const buttonElement = this.getButtonDOMElement();
         const buttonElementDimensions = buttonElement.getBoundingClientRect();
         const top = buttonElementDimensions.top;
         const left = buttonElementDimensions.left;
-
       }
       // this.setState({
       //   isSelected: true,
@@ -102,38 +120,61 @@ class DropdownMenuBarItem extends React.Component {
   }
 
   /**
-   * Handle key down events on the hyperlink
-   * @param {Event} event 
+   * Resets the CSS styling hover changes for the title and arrow elements when the user
+   * moves the mouse pointer away from the element again
    */
-  // handleOnKeyDown(event) {
-  //   if (event.key === ' ') {
-  //     /* Ensure that a spacebar key press also correctly redirects the user to the specified URL */
-  //     event.target.click();
-  //   }
-  // }
+  handleOnMouseLeave() {
+    const titleElement = this.getTitleDOMElement();
+    const arrowElement = this.getArrowDOMElement();
+
+    /* Reset the padding for the title element */
+    titleElement.style.paddingLeft = '5px';
+
+    /* Reset the width of the arrow element and remove the arrow icon */
+    arrowElement.style.minWidth = '5px';
+    arrowElement.style.width = '5px';
+    arrowElement.innerHTML = '';
+  }
 
   /**
-   * Handles the height of the menu item link text content. The height of the text content
-   * should not exceed the height of the item container itself.
+   * When the user hovers over the title and arrow elements, change the styling to
+   * display the arrow icon by altering the CSS styling of both elements.
    */
-  // handleTextContentHeight() {
-  //   /* Retrieve the span element from the DOM and determine its height and text content */
-  //   const spanElement = this.getSpanDOMElement();
-  //   let spanHeight = spanElement.getBoundingClientRect().height;
-  //   let spanTextContent = spanElement.textContent;
-  //   while (spanTextContent.length > 0 && spanHeight > maximumMenuItemLinkHeight) {
-  //     /* Remove the last character in the string and add three dots to the string end to suggest truncation has occurred */
-  //     spanTextContent = `${spanTextContent.substring(0, spanTextContent.length - 1).trim()}...`;
+  handleOnMouseOver() {
+    const titleElement = this.getTitleDOMElement();
+    const arrowElement = this.getArrowDOMElement();
 
-  //     /* Set the new text content string and determine the new height of the element */
-  //     spanElement.textContent = spanTextContent;
-  //     spanHeight = spanElement.getBoundingClientRect().height;
-  //     if (spanHeight > maximumMenuItemLinkHeight) {
-  //       /* Remove the obsolete three dots at the end of the string for the next iteration of the loop */
-  //       spanTextContent = spanTextContent.substring(0, spanTextContent.length - 3).trim();
-  //     }
-  //   }
-  // }
+    /* Remove the padding from the title element */
+    titleElement.style.paddingLeft = '0px';
+
+    /* Increase the width of the arrow element and render the arrow icon */
+    arrowElement.style.minWidth = '10px';
+    arrowElement.style.width = '10px';
+    arrowElement.innerHTML = '&darr;';
+  }
+
+  /**
+   * Handles the height of the dropdown menu items title text content. The height of the text content
+   * should not exceed the height of the button container element.
+   */
+  handleTextContentHeight() {
+    /* Retrieve the title text span element from the DOM and determine its height and text content */
+    const spanElement = this.getTitleDOMElement();
+    let spanHeight = spanElement.getBoundingClientRect().height;
+    let spanTextContent = spanElement.textContent;
+    while (spanTextContent.length > 0 && spanHeight > maximumMenuBarItemButtonHeight) {
+      /* Remove the last character in the string and add three dots to the string end to suggest truncation has occurred */
+      spanTextContent = `${spanTextContent.substring(0, spanTextContent.length - 1).trim()}...`;
+
+      /* Set the new text content string and determine the new height of the element */
+      spanElement.textContent = spanTextContent;
+      spanHeight = spanElement.getBoundingClientRect().height;
+      if (spanHeight > maximumMenuBarItemButtonHeight) {
+        /* Remove the obsolete three dots at the end of the string for the next iteration of the loop */
+        spanTextContent = spanTextContent.substring(0, spanTextContent.length - 3).trim();
+      }
+    }
+  }
 
   /**
    * Sets the dropdown menu bar items button as hidden in the UI 
@@ -175,34 +216,20 @@ class DropdownMenuBarItem extends React.Component {
     if (this.props.addRightSideSpacing === true) {
       buttonCss += ' menu-bar-item-container-right-side-spacing';
     }
+    /* Set the styling for the button and arrow elements */
     const buttonTitleCss = 'dropdown-menu-bar-button-title';
     const buttonArrowCss = 'dropdown-menu-bar-button-arrow';
 
-    // /* Set the styling for the internal hyperlink element */
-    // const hyperlinkCss = `menu-bar-item-hyperlink background-${backgroundColour} font-default font-${fontColour}`;
-
-    // /* Set the styling for the hyperlinks internal text content */
-    // const hyperlinkInnerContentCss = 'menu-bar-item-inner-content';
-
     return (
       <React.Fragment>
-        <button className={buttonCss} id={this.getIdButtonDOMElement()}>
-          <span id={`${this.props.dropdownMenuBarItemData.id}--${this.props.backgroundColour || 'white'}--title--dropdown-menu-bar-item`} className={buttonTitleCss}>
-            {this.props.dropdownMenuBarItemData.title}
-          </span>
-          <span className={buttonArrowCss}>
-            &darr;
-          </span>
+        <button className={buttonCss} id={this.getIdButtonDOMElement()} onBlur={this.handleOnMouseLeave} onClick={this.handleOnClickDropdownMenuBarItem}
+          onFocus={this.handleOnMouseOver} onMouseLeave={this.handleOnMouseLeave} onMouseOver={this.handleOnMouseOver} tabIndex={this.state.isHidden === true ? '-1' : '0'}>
+            <span className={buttonTitleCss}>
+              {this.props.dropdownMenuBarItemData.title}
+            </span>
+            <span className={buttonArrowCss} />
         </button>
       </React.Fragment>
-
-      // <div className={containerCss} id={`${this.props.id}--menu-bar-item`}>
-      //   <a aria-hidden={`${this.state.isHidden}`} aria-label={this.props.children} className={hyperlinkCss} href={`${this.props.href}`} 
-      //     id={`${this.props.id}--menu-bar-item-hyperlink`} onClick={this.handleOnClick} onKeyDown={this.handleOnKeyDown} role="link"
-      //     tabIndex={this.state.isHidden === true ? '-1' : '0'} title={this.props.children}>
-      //       <span className={hyperlinkInnerContentCss}>{this.props.children}</span>
-      //   </a>
-      // </div>
     );
   }
 }
@@ -213,6 +240,7 @@ DropdownMenuBarItem.propTypes = {
   backgroundColour: PropTypes.oneOf([ 'gold', 'green', 'grey', 'navy-and-gold', 'navy-and-white', 'red', 'white' ]),
   /** The dropdown menu bar items text content. */
   children: PropTypes.string,
+  /** The dropdown menu bar item data used to render the menu bar item and the links in the dropdown menu bar container element. */
   dropdownMenuBarItemData: PropTypes.shape({
     dropdownMenuBarItemsList: PropTypes.arrayOf(
       PropTypes.shape({
