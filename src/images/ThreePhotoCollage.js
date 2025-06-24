ce5cae5510c9b5eafc7a7ff6ea411da3ed90fd7f
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-24 08:44:38 +0100

    finalise three photo collage component

--- src/images/ThreePhotoCollage.js
+++ src/images/ThreePhotoCollage.js
@@ -3,16 +3,19 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import DecorativeImage from './DecorativeImage';
 import InformativeImage from './InformativeImage';
 import './css/image-three-photo-collage.css';
 
-// 1280 - 32 (desktop) = 1248px
-// 360 - 32 (mobile)   = 328px
+const initialMarginTop = 65;
+const maximumImageWidth = 620;
 
-// 8px spacing between images = 1240px (full width) or 320px (smallest)
-// Images sized at 620px (full width) or 160px (smallest)
-
+/**
+ * Three Photo Collage component which is intended for use inside the Main component in the web application. Renders three photos in a collage
+ * with two photos being placed side by side in the upper section of the collage and a single photo overlapping them slightly in the lower section.
+ * 
+ * This component auto-handles screen widths from the most commonly used mobile screen sizes (360x800) to the most commonly used desktop sizes (1920x1080)
+ * and will automatically adjust the heights and widths of the images to suit the current screen size.
+ */
 class ThreePhotoCollage extends React.Component {
   /**
    * Constructor for the Three Photo Collage component
@@ -24,15 +27,63 @@ class ThreePhotoCollage extends React.Component {
       height: 0,
       width: 0,
     };
+    this.getIdLowerImageContainerDOMElement = this.getIdLowerImageContainerDOMElement.bind(this);
+    this.getIdRootContainerDOMElement = this.getIdRootContainerDOMElement.bind(this);
     this.getIdUpperImagesContainerDOMElement = this.getIdUpperImagesContainerDOMElement.bind(this);
+    this.getLowerImageContainerDOMElement = this.getLowerImageContainerDOMElement.bind(this);
+    this.getRootContainerDOMElement = this.getRootContainerDOMElement.bind(this);
     this.getUpperImageRightContainerDOMElement = this.getUpperImageRightContainerDOMElement.bind(this);
     this.handleWindowResize = this.handleWindowResize.bind(this);
   }
 
   componentDidMount() {
-    this.handleWindowResize();
+    /* Auto handle the initial screen size after a slight pause to give the page time to load */
+    setTimeout(() => {
+      this.handleWindowResize(true);
+    }, 50);
 
+    /* For all future screen resize events, ensure that the image dimensions are handled correctly */
     window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  /**
+   * Retrieves the ID set to the lower image container element
+   * @returns {string}
+   */
+  getIdLowerImageContainerDOMElement() {
+    return `${this.props.id}--lower-section--three-photo-collage`;
+  }
+
+  /**
+   * Retrieves the ID set to the root container element
+   * @returns {string}
+   */
+  getIdRootContainerDOMElement() {
+    return `${this.props.id}--three-photo-collage`;
+  }
+
+  /**
+   * Retrieves the ID set to the upper images container element
+   * @returns {string}
+   */
+  getIdUpperImagesContainerDOMElement() {
+    return `${this.props.id}--upper-section--three-photo-collage`;
+  }
+
+  /**
+   * Retrieves the DOM element for the lower image container element
+   * @returns {HTMLElement}
+   */
+  getLowerImageContainerDOMElement() {
+    return document.querySelector(`div[id="${this.getIdLowerImageContainerDOMElement()}"]`);
+  }
+
+  /**
+   * Retrieves the DOM element for the root container element
+   * @returns {HTMLElement}
+   */
+  getRootContainerDOMElement() {
+    return document.querySelector(`div[id="${this.getIdRootContainerDOMElement()}"]`);
   }
 
   /**
@@ -44,20 +95,35 @@ class ThreePhotoCollage extends React.Component {
   }
 
   /**
-   * Retrieves the ID set to the upper images container element
-   * @returns {string}
+   * Handler for screen resize events - the images width and height need to be determined based on the new screen size
    */
-  getIdUpperImagesContainerDOMElement() {
-    return `${this.props.id}--three-photo-collage`;
-  }
-
-  handleWindowResize() {
+  handleWindowResize(onLoadPage = false) {
     const rightUpperImageContainerElement = this.getUpperImageRightContainerDOMElement();
     if (rightUpperImageContainerElement !== null) {
-      const currentImageWidth = parseInt(rightUpperImageContainerElement.getBoundingClientRect().width, 10);
+      /* Get the full width of the image container */
+      const imageContainerWidth = parseInt(rightUpperImageContainerElement.getBoundingClientRect().width, 10);
+
+      /* Get the lower image container element */
+      const lowerImageContainerElement = this.getLowerImageContainerDOMElement();
+
+      /* For every 15 pixels less than the maximum permitted width - reduce the margin top allowance by 1px */
+      const imagePixelReduction = maximumImageWidth - imageContainerWidth;
+      const marginTopPixelReduction = parseInt(imagePixelReduction / 15);
+      const marginTop = `-${initialMarginTop - marginTopPixelReduction}px`;
+
+      /* Set the new margin top CSS value to the lower image container element */
+      lowerImageContainerElement.style.marginTop = marginTop;
+      
+      /* Set the height and width of the images based on the current container width */
       this.setState({
-        height: parseInt(currentImageWidth * 0.75, 10),
-        width: currentImageWidth,
+        height: parseInt(imageContainerWidth * 0.75, 10),
+        width: imageContainerWidth,
+      }, () => {
+        /* If this is the first time we have loaded the page - blend in the photos now everything has been loaded and set */
+        if (onLoadPage === true) {
+          const rootContainerElement = this.getRootContainerDOMElement();
+          rootContainerElement.style.opacity = 1;
+        }
       });
     }
   }
@@ -76,10 +142,14 @@ class ThreePhotoCollage extends React.Component {
     const upperImageRightContainerCss = 'three-photo-collage-image-container';
 
     /* Set the styling for the lower image container element */
-    const lowerImageContainerCss = 'three-photo-collage-lower-image-container three-photo-collage-lower-image-container-spacing';
+    const lowerImageContainerCss = 'three-photo-collage-lower-image-container';
+
+    /* */
+    let lowerImageRenderCss = 'three-photo-collage-lower-image-render';
+    this.props.borderColour === 'grey' ? lowerImageRenderCss += ' three-photo-collage-image-border-grey' : lowerImageRenderCss += ' three-photo-collage-image-border-white';
 
     return (
-      <div className={rootCss}>
+      <div className={rootCss} id={this.getIdRootContainerDOMElement()}>
         <div className={upperImagesContainerCss} id={this.getIdUpperImagesContainerDOMElement()}>
           <div className={upperImageLeftContainerCss}>
             <InformativeImage alt={this.props.imageUpperLeftData.alt} height={this.state.height} id={this.props.id} src={this.props.imageUpperLeftData.src}
@@ -90,24 +160,31 @@ class ThreePhotoCollage extends React.Component {
               width={this.state.width} />
           </div>
         </div>
-        <div className={lowerImageContainerCss}>
-          <InformativeImage alt={this.props.imageLowerData.alt} height={this.state.height} id={this.props.id} src={this.props.imageLowerData.src} width={this.state.width} />
+        <div className={lowerImageContainerCss} id={this.getIdLowerImageContainerDOMElement()}>
+          <div className={lowerImageRenderCss}>
+            <InformativeImage alt={this.props.imageLowerData.alt} height={this.state.height} id={this.props.id} src={this.props.imageLowerData.src} width={this.state.width} />
+          </div>
         </div>
       </div>
     );
   }
 }
 ThreePhotoCollage.propTypes = {
+  /** The border colour used for the collage. Ideally you would set this to match the background colour of the Main component. The default colour for the border is white. */
+  borderColour: PropTypes.oneOf([ 'white', 'grey' ]),
   /** The unique identifier for this component. */
   id: PropTypes.string.isRequired,
+  /** The data for the image to be rendered centrally in the lower section of the collage. */
   imageLowerData: PropTypes.shape({
     alt: PropTypes.string,
     src: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]),
   }).isRequired,
+  /** The data for the image to be rendered to the left side of the upper section of the collage. */
   imageUpperLeftData: PropTypes.shape({
     alt: PropTypes.string,
     src: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]),
   }).isRequired,
+  /** The data for the image to be rendered to the right side of the upper section of the collage. */
   imageUpperRightData: PropTypes.shape({
     alt: PropTypes.string,
     src: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]),
