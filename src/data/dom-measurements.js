6f830981ada9a94a77ed4d4744f96d66a606ae3f
Anthony Cox <anthonyjohnc@gmail.com>
2025-06-24 12:47:57 +0100

    add common dom element measurements functionality

--- src/data/dom-measurements.js
+++ src/data/dom-measurements.js
@@ -3,42 +3,46 @@
  */
 
 /**
- * 
- * @param {*} number1 
- * @param {*} number2 
- * @returns 
+ * Divides two numbers and parses the answer to an integer value
+ * @param {number} number1 
+ * @param {number} number2 
+ * @returns {number}
  */
 const divide_Integer = (number1, number2) => {
   return parseInt_Default(number1 / number2);
 };
 
 /**
- * 
+ * Retrieves the dimensions for the specified DOM element and parses all
+ * dimension values to an integer
  * @param {HTMLElement} domElement 
- * @returns {{ width: number }}
+ * @returns {{ bottom: number, height: number, left: number, right: number, width: number }}
  */
 const getBoundingClientRect = domElement => {
   const dimensions = domElement.getBoundingClientRect();
-
   return {
+    bottom: parseInt_Default(dimensions.bottom),
+    height: parseInt_Default(dimensions.height),
+    left: parseInt_Default(dimensions.left),
+    right: parseInt_Default(dimensions.right),
     width: parseInt_Default(dimensions.width),
   };
 };
 
 /**
- * 
- * @param {*} number1 
- * @param {*} number2 
- * @returns 
+ * Multiplies two numbers and parses the answer to an integer value
+ * @param {number} number1 
+ * @param {number} number2 
+ * @returns {number}
  */
 const multiply_Integer = (number1, number2) => {
   return parseInt_Default(number1 * number2);
 };
 
 /**
- * 
- * @param {*} number 
- * @returns 
+ * Parses the specified number to an integer using the default / common radix
+ * @param {number} number 
+ * @returns {number}
  */
 const parseInt_Default = number => {
   return parseInt(number, 10);
@@ -48,4 +52,5 @@ export {
   divide_Integer,
   getBoundingClientRect,
   multiply_Integer,
+  parseInt_Default,
 };
